68f37ffe35375206f030df64f7bd8f6305b6703d
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-04-05 10:23:10 +0200

    Add Square skeleton and add junit (#2)

---
+++ src/main/java/ch/heigvd/gen/labo3/Square.java
@@ -0,0 +1,13 @@
+package ch.heigvd.gen.labo3;
+
+public class Square {
+    private String name;
+
+    Square(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
