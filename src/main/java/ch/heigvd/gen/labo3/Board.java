cf65a38dd50a0e22f84cd40fd5ba45da47acfee5
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-04-05 11:57:39 +0200

    Implement getSquare with movement fixes #14 fixes #15

--- src/main/java/ch/heigvd/gen/labo3/Board.java
+++ src/main/java/ch/heigvd/gen/labo3/Board.java
@@ -1,17 +1,24 @@
 package ch.heigvd.gen.labo3;
 
+import java.util.ArrayList;
+
 public class Board {
-    private Square map[];
+    private final int size = 40;
+    private ArrayList<Square> map;
 
     public Board() {
-        map = new Square[40];
+        map = new ArrayList<>(size);
 
-        for(Integer i = 0; i < 40; i++) {
-            map[i] = new Square("square#" + i.toString());
+        for(Integer i = 0; i < size; i++) {
+            map.add(new Square("square#" + i.toString()));
         }
     }
 
     public Square getSquare() {
-        return map[0];
+        return map.get(0);
+    }
+
+    public Square getSquare(Square current, int step) {
+        return map.get((map.indexOf(current) + step)%size);
     }
 }
--- src/test/java/ch/heigvd/gen/labo3/BoardTest.java
+++ src/test/java/ch/heigvd/gen/labo3/BoardTest.java
@@ -11,4 +11,29 @@ public class BoardTest {
 
         assertEquals("square#0", board.getSquare().getName());
     }
+
+    @Test
+    void shouldGiveNextStep() {
+        Board board = new Board();
+        Square current = board.getSquare();
+
+        for(Integer i = 1; i < 40; ++i) {
+            current = board.getSquare(current, 1);
+
+            assertEquals(current.getName(), new String("square#") + i.toString());
+        }
+    }
+
+    @Test
+    void shouldGiveNextStepAtEnd() {
+        Board board = new Board();
+        Square current = board.getSquare();
+
+        current = board.getSquare(current, 39);
+        assertEquals(current.getName(), "square#39");
+
+        current = board.getSquare(current, 1);
+        assertEquals(current.getName(), "square#0");
+
+    }
 }
