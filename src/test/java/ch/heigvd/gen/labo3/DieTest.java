70748ec7ef391111340bb6b787358d404c3aa8f0
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-04-05 11:31:51 +0200

    make meaven happy

--- src/test/java/ch/heigvd/gen/labo3/DieTest.java
+++ src/test/java/ch/heigvd/gen/labo3/DieTest.java
@@ -2,9 +2,6 @@ package ch.heigvd.gen.labo3;
 
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import sun.jvm.hotspot.utilities.Assert;
-
-import java.lang.reflect.Array;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
