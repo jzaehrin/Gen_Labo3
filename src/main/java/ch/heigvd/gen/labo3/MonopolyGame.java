f42062be7898ac6dc7d9aa84adc0468ad507cc4d
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2019-04-12 11:04:33 +0200

    Implement MGame (#18)
    * Implement MGame
    
    * Add main

---
+++ src/main/java/ch/heigvd/gen/labo3/MonopolyGame.java
@@ -0,0 +1,39 @@
+package ch.heigvd.gen.labo3;
+
+import java.util.ArrayList;
+
+public class MonopolyGame {
+    private int nbRound;
+    private int roundCnt;
+    private Board board;
+    private ArrayList<Player> players;
+    private Die[] dice;
+
+    public MonopolyGame(int nbPlayer, int nbRound) {
+        this.nbRound = nbRound;
+        this.roundCnt = 0;
+
+        this.board = new Board();
+
+        this.dice = new Die[2];
+        this.dice[0] = new Die();
+        dice[1] = new Die();
+
+        players = new ArrayList<>(nbPlayer);
+        for(int i = 0; i < nbPlayer; i++)
+            players.add(new Player(String.format("player#%d", i), board, dice));
+    }
+
+    public void playGame() {
+        for(roundCnt = 0; roundCnt < nbRound; roundCnt++) {
+            System.out.println(String.format("Starting turn #%d", roundCnt));
+            playRound();
+        }
+    }
+
+    private void playRound() {
+        for(Player player: players) {
+            player.takeTurn();
+        }
+    }
+}
