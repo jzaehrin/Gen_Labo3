aafc6ef2e53a6562aea4728e26e31677d2b4a94e
jerozerbib <jerozerbib@gmail.com>
2019-04-12 11:30:15 +0200

    spec de GoSquare fixes #25

--- src/test/java/ch/heigvd/gen/labo3/GoSquareTest.java
+++ src/test/java/ch/heigvd/gen/labo3/GoSquareTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RegularSquareTest {
+public class GoSquareTest {
     @Test
-    void regularSquareShouldHaveAName() {
-        String name = "RegularSquare";
+    void goSquareShouldHaveAName() {
+        String name = "GoSquare";
 
         Square square = new RegularSquare(name);
 
